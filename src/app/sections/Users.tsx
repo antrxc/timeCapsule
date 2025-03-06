d047afd6f2d653aad918afaaffd422e541714948
antrxc <uiantric06@gmail.com>
2025-03-06 12:34:09 +0000

    Added and optimised the Users component

--- src/app/sections/Users.tsx
+++ src/app/sections/Users.tsx
@@ -1,38 +1,19 @@
 import { cn } from "@/lib/utils";
 import { SparklesText } from "@/components/magicui/sparkles-text";
-import { Marquee } from "@/components/magicui/marquee";
+import names from "@/names/names";
 
 export function Users() {
-    const names = [
-        "Aaliyah", "Aaron", "Abigail", "Adam", "Addison", "Adrian", "Aiden", "Alan", "Alex", "Alexandra",
-        "Alexis", "Alice", "Allison", "Amelia", "Amy", "Andrew", "Angel", "Angela", "Anna", "Anthony",
-        "Ariana", "Ariel", "Arthur", "Ashley", "Aubrey", "Audrey", "Austin", "Ava", "Avery", "Bailey",
-        "Benjamin", "Blake", "Brandon", "Brayden", "Brianna", "Caleb", "Cameron", "Caroline", "Carter", "Chloe",
-        "Christian", "Christopher", "Claire", "Cody", "Colin", "Connor", "Cooper", "Daniel", "David", "Declan",
-        "Dylan", "Eleanor", "Elijah", "Elizabeth", "Ella", "Emily", "Emma", "Ethan", "Eva", "Evan",
-        "Evelyn", "Gabriel", "Gabriella", "Grace", "Grayson", "Hailey", "Hannah", "Harper", "Harrison", "Hazel",
-        "Henry", "Hudson", "Hunter", "Ian", "Isaac", "Isabella", "Jack", "Jackson", "Jacob", "James",
-        "Jasmine", "Jason", "Jayden", "Jennifer", "Jessica", "John", "Jonathan", "Jordan", "Joseph", "Joshua",
-        "Julia", "Julian", "Justin", "Kai", "Kate", "Katherine", "Kayla", "Kaylee", "Kevin", "Kyle",
-        "Lauren", "Leo", "Liam", "Lillian", "Lily", "Logan", "Lucas", "Lucy", "Luke", "Madison",
-      ];
+
 
       const firstRow = names.slice(0, names.length / 2);
       const secondRow = names.slice(names.length / 2);
-  return (
-    <div className="relative flex w-full flex-col items-center justify-center overflow-hidden">
-               <Marquee pauseOnHover className="[--duration:20s]">
-                {firstRow.map((review) => (
-                <SparklesText text={review} key={review} className="text-2xl text-gray-600 " sparklesCount={1}/>
-                ))}
-            </Marquee>
-            <Marquee reverse pauseOnHover className="[--duration:20s]">
-                {secondRow.map((review) => (
-                <SparklesText text={review} key={review} className="text-2xl text-gray-600 " sparklesCount={1}  />
-                ))}
-            </Marquee>
-            <div className="pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-gradient-to-r from-background"></div>
-      <div className="pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-gradient-to-l from-background"></div>
-    </div>
+    return (
+      <div className="grid grid-cols-5 gap-4 w-fit ml-auto mr-auto">
+                        {names.map((review) => (
+                          <div className="ml-auto mr-auto" key={review}>
+                            <SparklesText key={review} text={review} className="text-2xl text-gray-300 w-fit hover:text-black " sparklesCount={1} />
+                          </div>
+        ))}
+       </div>
   );
 }
